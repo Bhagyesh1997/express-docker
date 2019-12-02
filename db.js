ae8be20aa3df5562e01324df921a98ca695a8a23
bhagyesh <bhagyeshpatil05@gmail.com>
2019-12-02 14:21:25 +0530

    second commit

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 function connect()
 {
     const connection = mysql.createConnection({
-        host: '192.168.1.223',
+        host: '172.18.5.28',
         user: 'root',
         password: 'root',
         database: 'myapp_db',
